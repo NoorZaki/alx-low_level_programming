1be0a67d6d66a9dc57277ba740f904062b363e66
NoorZaki <noormagdi.2013@gmail.com>
2023-06-16 13:30:08 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,9 @@
  */
 int main(void)
 {
-int digit = 0;
-while (digit <= 9)
-{
-putchar(digit + '0');
-digit++;
-}
+int n;
+for (n = 0; n < 10; n++)
+putchar((n % 10) + '0');
 putchar('\n');
 return (0);
 }
