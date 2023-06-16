f34279fb7ee33a5bc6d21d502dc1dac2d32622e8
NoorZaki <noormagdi.2013@gmail.com>
2023-06-16 07:51:20 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* main - entry point
- *
- * Return: Always 0 (success)
- *
- */
-
-/* Determines if anumber is positive , negative or Zero */
+*
+* Description: Determines if anumber is positive , ngative or Zero
+*
+* Return: Always 0 (success)
+*/ 
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>0)
-	{
-		printf("%d is positive\n",n);
-	}
-	else if (n==0)
-	{
-		printf("%d is zero\n",n);
-	}
-	else 
-	{
-		printf("%d is negative\n",n);
-	}
+	if(n>0)
+		printf("%d is positive\n", n);
+	else if(n==0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
 	return (0);
 }
