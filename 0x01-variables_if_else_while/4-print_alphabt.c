e7294fcf7870577d4adbba095d42d35f06a69e20
NoorZaki <noormagdi.2013@gmail.com>
2023-06-16 13:03:19 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,13 @@
  */
 int main(void)
 {
-for (i = 'a'; i <= 'z'; i++)
+char ch = 'a';
+while (ch <= 'z')
 {
-if (letter != 'e' && letter != 'q')
-{
-putchar(i);
-}
+if (ch == 'e' || ch == 'q')
+ch++;
+putchar(ch);
+ch++;
 }
 putchar('\n');
 return (0);
