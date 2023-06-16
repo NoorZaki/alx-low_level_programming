20050750ad4f1205cfae19fc24e22d3e75c9b9c5
NoorZaki <noormagdi.2013@gmail.com>
2023-06-16 12:33:23 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,10 +8,12 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-for (i = 0; i < 52; i++)
+for (i = 97; i < 123; i++)
 {
-putchar(alp[i]);
+if (i != 101 && i != 113)
+{
+putchar(i);
+}
 }
 putchar('\n');
 return (0);
