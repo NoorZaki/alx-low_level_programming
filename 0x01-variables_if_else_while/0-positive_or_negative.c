7c4c16b1980306d371a3b521589d3cf3c746eadf
NoorZaki <noormagdi.2013@gmail.com>
2023-06-16 08:58:22 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,23 +1,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* 
- * main - entry point
- *
- * Description: Determines if anumber is positive , ngative or Zero
- *
- * Return: Always 0 (success)
+/**
+ * main - Entry point
+ * Description: 'print all possible different combinations of tow digits'
+ * Return: always 0
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	else if(n == 0)
+	}
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else
+	{
 		printf("%d is negative\n", n);
+	}
 	return (0);
 }
