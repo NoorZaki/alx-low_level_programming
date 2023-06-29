f75cf5852264b6fa1d534981844146fbeb390fc4
NoorZaki <noormagdi.2013@gmail.com>
2023-06-29 06:23:19 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string in rot13
- * @s: string to be encoded
+ * rot13 - translates string to ROT13
+ * @s: input string to be translated
  *
- * Return: the resulting string
+ * Return: string after conversion of ROT13
  */
+
 char *rot13(char *s)
 {
 int i, j;
-char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char rot_it[] =   "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 for (i = 0; s[i] != '\0'; i++)
-{
-for (j = 0; a[j] != '\0'; j++)
-{
-if (s[i] == a[j])
-{
-s[i] = b[j];
-break
-}
-}
-}
+for (j = 0; j < 52; j++)
+if (s[i] == alphabet[j])
+s[i] = rot_it[j], j = 52;
 return (s);
 }
