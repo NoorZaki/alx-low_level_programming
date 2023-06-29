4afb7fa3aa3a1198d50f240e4cee48347270fd2b
NoorZaki <noormagdi.2013@gmail.com>
2023-06-29 07:10:59 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-/*
- * main - entry point
- *
- * Return: Alwyas 0 (success)
- */
+
 int main(void)
 {
 int n;
@@ -12,12 +8,12 @@ int *p;
 a[2] = 1024;
 p = &n;
 /*
-* write yout line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statment
-* - you are not allowed to code anything else than this line of code
+ * write your line of code here...
+ * Remember:
+ * - you are not allowed to use a
+ * - you are not allowed to modify p
+ * - only one statement
+ * - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
