d822a934329d1d3e98b23475748f4ef9eb9fba96
NoorZaki <noormagdi.2013@gmail.com>
2023-07-19 11:15:22 -0700

    100

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,36 +2,38 @@
 #include <stdlib.h>
 
 /**
- * main - prints its own opcodes
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Prints the opcodes of this function
  *
- * Return: Always 0 (Success)
+ * @argc: The number of command-line arguments
+ *
+ * @argv: The command-line arguments
+ *
+ * Return: 0 if successful
  */
+
 int main(int argc, char *argv[])
 {
-int bytes, i;
-char *arr;
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-bytes = atoi(argv[1]);
-if (bytes < 0)
-{
-printf("Error\n");
-exit(2);
-}
-arr = (char *)main;
-for (i = 0; i < bytes; i++)
-{
-if (i == bytes - 1)
-{
-printf("%02hhx\n", arr[i]);
-break;
-}
-printf("%02hhx ", arr[i]);
-}
-return (0);
+	int n_bytes, i;
+	int (*prog)(int, char **) = &main;
+
+	if (argc == 2)
+	{
+		n_bytes = atoi(argv[1]);
+		if (n_bytes < 0)
+		{
+			printf("Error\n");
+			exit(2);
+		}
+		for (i = 0; i < n_bytes; i++)
+			printf("%02x%c",
+					(unsigned char)*((char *)prog + i), i < n_bytes - 1 ? ' ' : '\n');
+	}
+	else
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	return (0);
+
 }
